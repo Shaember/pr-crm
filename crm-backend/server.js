695987313c1faa413fcb777e2f1f599e4cddca77
Anifost <amiribragimov128@gmail.com>
2026-05-21 05:38:32 +0300

    add /health and /metrics endpoints, add prom-client

--- crm-backend/server.js
+++ crm-backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const client = require('prom-client');
 const clientsRoutes = require("./routes/clients");
 const authRoutes = require("./routes/auth");
 const app = express();
@@ -9,6 +10,14 @@ app.use(express.json());
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
+});
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
 });
 
 app.use("/api/auth", authRoutes);
